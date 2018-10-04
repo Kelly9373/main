0d9f82f191f799b00bfbd1287b1aff7d0affc57b
Liu Xiaohang <>
2018-10-04 17:43:46 +0800

    Correct checkstyle errors in LoanRateTest

--- src/test/java/seedu/address/model/person/LoanRateTest.java
+++ src/test/java/seedu/address/model/person/LoanRateTest.java
@@ -1,17 +1,5 @@
 package seedu.address.model.person;
 
-import org.junit.Test;
-
-import static org.junit.Assert.*;
-
 // TODO: Add test cases
 public class LoanRateTest {
-    /*
-    @Test
-    public void isValidRate() {
-    }
-
-    @Test
-    public void equals() {
-    }*/
 }
